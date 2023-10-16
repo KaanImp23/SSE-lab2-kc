1c608fea5f9b2f2780655368591a00673ca38ae6
Kaan Chitranapawong <kaan.chitranapawong23@imperial.ac.uk>
2023-10-16 21:21:44 +0100

    Fourth

--- api/app.py
+++ api/app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
-
 @app.route("/submit", methods=["POST"])
 
 def submit():
