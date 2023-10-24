9a67ad35ff6c1f92b22adfae4a397b05743318ee
Kaan Chitranapawong <kaan.chitranapawong23@imperial.ac.uk>
2023-10-24 12:33:53 +0100

    Fixed format app.py

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
@@ -16,13 +17,15 @@ def submit():
             age=input_age, colour=input_fav_colour
             )
 
+
 def process_query(query):
     if query == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
+
+
     else:
         return "Unknown"
 @app.route('/query', methods=['GET'])
 def query_route():
     q_parameter = request.args.get('q')
     process_query(q_parameter)
-
