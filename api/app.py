29ccfb6091f4005ce8d2e90be44699fe4f7c1f80
Kaan Chitranapawong <kaan.chitranapawong23@imperial.ac.uk>
2023-11-06 15:36:37 +0000

    Added request API

--- api/app.py
+++ api/app.py
@@ -14,9 +14,11 @@ def submit():
     input_age = request.form.get("age")
     input_fav_colour = request.form.get("colour")
     input_git_username = request.form.get("username")
-    response = requests.get("https://api.github.com/users/input_git_username/repos")
+    response = requests.get(
+            "https://api.github.com/users/input_git_username/repos"
+            )
     if response.status_code == 200:
-        repos = response.json() 
+        repos = response.json()
         for repo in repos:
             print(repo["full_name"])
     return render_template(
