0224a359ec5e4490a0c5bc3864a2a9af910236cb
Kaan Chitranapawong <kaan.chitranapawong23@imperial.ac.uk>
2023-11-07 22:24:06 +0000

    Added Details

--- api/app.py
+++ api/app.py
@@ -19,30 +19,78 @@ def submit():
             )
     if response.status_code == 200:
         repos = response.json()
+        repo_list = []
         repo_name = []
-        repo_update = []
-        com_list = []
+        sentence_list = []
         for repo in repos:
+            repo_list.append(repo["full_name"])
             repo_name.append(repo["full_name"])
-            repo_update.append(repo["updated_at"])
-            commits = requests.get(
-                "https://api.github.com/repos/" + repo["full_name"] +
-                "/commits?per_page=5"
-            )
-            if commits.status_code == 200:
-                coms = commits.json()
-                for com in coms:
-                    com_list.append(com["commit"]["tree"]["sha"])
-                    com_list.append(com["commit"]["author"]["name"])
-                    com_list.append(com["commit"]["committer"]["date"])
-                    com_list.append(com["commit"]["message"])
-        repo_name1 = repo_name[0]
-        repo_name2 = repo_name[1]
+            repo_list.append(repo["updated_at"])
+        dict_repo = {}
+        for i in range(0, len(repo_list), 2):
+            dict_repo[repo_list[i]] = repo_list[i + 1]
+        for k,v in dict_repo.items():
+            sentence = "Repository: " + str(k) + \
+            " was last updated at " + v
+            sentence_list.append(sentence)
     return render_template(
             "hello.html", name=input_name,
             age=input_age, colour=input_fav_colour,
-            username=input_git_username, name1=repo_name1,
-            name2=repo_name2,com=com_list
+            username=input_git_username, dictrepo=dict_repo,
+            sentence=sentence_list
+            )
+
+@app.route('/submit/detail', methods=["POST"])
+def details():
+    repo_name = request.form.get("reponame")
+    commit_response = requests.get(
+            "https://api.github.com/repos/" + repo_name + 
+            "/commits?per_page=5"
+            )
+    if commit_response.status == 200:
+        com_sha = []
+        com_author = []
+        com_date = []
+        com_message = []
+        com = commit_response.json()
+        for commit in com:
+            com_sha.append(commit["commit"]["tree"]["sha"])
+            com_author.append(commit["commit"]["author"]["name"])
+            com_date.append(commit["commit"]["committer"]["date"])
+            com_message.append(commit["commit"]["message"])
+        com_sha1 = com_sha[0]
+        com_sha2 = com_sha[1]
+        com_sha3 = com_sha[2]
+        com_sha4 = com_sha[3]
+        com_sha5 = com_sha[4]
+        com_author1 = com_author[0]
+        com_author2 = com_author[1]
+        com_author3 = com_author[2]
+        com_author4 = com_author[3]
+        com_author5 = com_author[4]
+        com_date1 = com_date[0]
+        com_date2 = com_date[1]
+        com_date3 = com_date[2]
+        com_date4 = com_date[3]
+        com_date5 = com_date[4]
+        com_message1 = com_message[0]
+        com_message2 = com_message[1]
+        com_message3 = com_message[2]
+        com_message4 = com_message[3]
+        com_message5 = com_message[4]
+        return render_template(
+            "commit_details.html", reponame=repo_name,
+            sha1=com_sha1, author1=com_author1,
+            date1=com_date1, message1=com_message1,
+            sha2=com_sha2, author2=com_author2,
+            date2=com_date2, message2=com_message2,
+            sha3=com_sha3, author3=com_author3,
+            date3=com_date3, message3=com_message3,
+            sha4=com_sha4, author4=com_author4,
+            date4=com_date4, message4=com_message4,
+            sha5=com_sha5, author5=com_author5,
+            date5=com_date5, message5=com_message5
+
             )
 
 
