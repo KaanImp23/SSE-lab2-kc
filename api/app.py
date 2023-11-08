b9fb215c9fa2acd988051f8ea554d2c19f3f4722
Kaan Chitranapawong <kaan.chitranapawong23@imperial.ac.uk>
2023-11-08 22:09:15 +0000

    Added Dynamic Table Content

--- api/app.py
+++ api/app.py
@@ -34,27 +34,28 @@ def chooserepo():
             )
     if response.status_code == 200:
         repos = response.json()
-        repo_list = []
         repo_name = []
-        sentence_list = []
+        repo_update = []
         for repo in repos:
-            repo_list.append(repo["full_name"])
             repo_name.append(repo["full_name"])
-            repo_list.append(repo["updated_at"])
-        dict_repo = {}
-        for i in range(0, len(repo_list), 2):
-            dict_repo[repo_list[i]] = repo_list[i + 1]
-        for k, v in dict_repo.items():
-            sentence = "Repository: " + str(k) + \
-                " was last updated at " + v
-            sentence_list.append(sentence)
-            separator = "           "
-            combined_sentence = separator.join(sentence_list)
+            repo_update.append(repo["updated_at"])
+        name_length = len(repo_name)
+        update_length = len(repo_update)
+        if name_length > 5:
+            name_length = 5
+        for i in range(name_length):
+            globals()[f"reponame{i}"] = repo_name[i]
+        if update_length > 5:
+            update_length = 5
+        for j in range(update_length):
+            globals()[f"repoupdate{j}"] = repo_update[j]
     return render_template(
             "hello_git_user.html", name=input_name,
-            colour=input_fav_colour,
-            username=input_git_username, dictrepo=dict_repo,
-            sentence=combined_sentence
+            colour=input_fav_colour,username=input_git_username,
+            reponame0=reponame0,reponame1=reponame1,reponame1=reponame1,
+            reponame1=reponame1,reponame1=reponame1,repoupdate0=repoupdate0,
+            repoupdate1=repoupdate1,repoupdate2=repoupdate2,
+            repoupdate3=repoupdate3,repoupdate4=repoupdate4
             )
 
 
@@ -76,38 +77,38 @@ def details():
             com_author.append(commit["commit"]["author"]["name"])
             com_date.append(commit["commit"]["committer"]["date"])
             com_message.append(commit["commit"]["message"])
-        com_sha1 = com_sha[0]
-        com_sha2 = com_sha[1]
-        com_sha3 = com_sha[2]
-        com_sha4 = com_sha[3]
-        com_sha5 = com_sha[4]
-        com_author1 = com_author[0]
-        com_author2 = com_author[1]
-        com_author3 = com_author[2]
-        com_author4 = com_author[3]
-        com_author5 = com_author[4]
-        com_date1 = com_date[0]
-        com_date2 = com_date[1]
-        com_date3 = com_date[2]
-        com_date4 = com_date[3]
-        com_date5 = com_date[4]
-        com_message1 = com_message[0]
-        com_message2 = com_message[1]
-        com_message3 = com_message[2]
-        com_message4 = com_message[3]
-        com_message5 = com_message[4]
+        sha_length = len(com_sha)
+        author_length = len(com_author)
+        date_length = len(com_date)
+        message_length = len(com_message)
+        if sha_length > 5:
+            sha_length = 5
+        if author_length > 5:
+            author_length = 5
+        if date_length > 5:
+            date_length = 5
+        if message_length > 5:
+            message_length = 5
+        for i in range(sha_length):
+            globals()[f"com_sha{i}"] = com_sha[i]
+        for i in range(author_length):
+            globals()[f"com_author{i}"] = com_author[i]
+        for i in range(date_length):
+            globals()[f"com_date{i}"] = com_date[i]
+        for i in range(message_length):
+            globals()[f"com_message{i}"] = com_message[i]
         return render_template(
             "commit_details.html", reponame=repo_name,
-            sha1=com_sha1, author1=com_author1,
-            date1=com_date1, message1=com_message1,
-            sha2=com_sha2, author2=com_author2,
-            date2=com_date2, message2=com_message2,
-            sha3=com_sha3, author3=com_author3,
-            date3=com_date3, message3=com_message3,
-            sha4=com_sha4, author4=com_author4,
-            date4=com_date4, message4=com_message4,
-            sha5=com_sha5, author5=com_author5,
-            date5=com_date5, message5=com_message5)
+            sha1=com_sha0, author1=com_author0,
+            date1=com_date0, message1=com_message0,
+            sha2=com_sha1, author2=com_author1,
+            date2=com_date1, message2=com_message1,
+            sha3=com_sha2, author3=com_author2,
+            date3=com_date2, message3=com_message2,
+            sha4=com_sha3, author4=com_author3,
+            date4=com_date3, message4=com_message3,
+            sha5=com_sha4, author5=com_author4,
+            date5=com_date4, message5=com_message4)
 
 
 @app.route('/query')
